2ff11ec7c5e1670feb46b0b7e98a4a042e5791e0
Dave James Miller <d@djm.me>
2022-10-08 18:34:01 +0100

    Add CAA records

---
+++ caa-records.tf
@@ -0,0 +1,47 @@
+resource "cloudflare_record" "djm_me_CAA_iodef" {
+  zone_id = cloudflare_zone.djm_me.id
+  name    = cloudflare_zone.djm_me.zone
+  type    = "CAA"
+
+  data {
+    flags = 0
+    tag   = "iodef"
+    value = "mailto:d@djm.me"
+  }
+}
+
+resource "cloudflare_record" "djm_me_CAA_letsencrypt" {
+  zone_id = cloudflare_zone.djm_me.id
+  name    = cloudflare_zone.djm_me.zone
+  type    = "CAA"
+
+  data {
+    flags = 0
+    tag   = "issue"
+    value = "letsencrypt.org"
+  }
+}
+
+resource "cloudflare_record" "davejamesmiller_com_CAA_iodef" {
+  zone_id = cloudflare_zone.davejamesmiller_com.id
+  name    = cloudflare_zone.davejamesmiller_com.zone
+  type    = "CAA"
+
+  data {
+    flags = 0
+    tag   = "iodef"
+    value = "mailto:d@djm.me"
+  }
+}
+
+resource "cloudflare_record" "davejamesmiller_com_CAA_letsencrypt" {
+  zone_id = cloudflare_zone.davejamesmiller_com.id
+  name    = cloudflare_zone.davejamesmiller_com.zone
+  type    = "CAA"
+
+  data {
+    flags = 0
+    tag   = "issue"
+    value = "letsencrypt.org"
+  }
+}
