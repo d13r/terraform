4345cda8203d9901de8663adbfc871163913b875
Dave James Miller <d@djm.me>
2022-11-04 23:01:38 +0000

    Move Mail Me to Seven

--- app-mail-me.tf
+++ app-mail-me.tf
@@ -2,5 +2,5 @@ resource "cloudflare_record" "mail_djm_me_CNAME" {
   zone_id = cloudflare_zone.djm_me.id
   name    = "mail"
   type    = "CNAME"
-  value   = "summer.djm.me"
+  value   = "seven.djm.me"
 }
