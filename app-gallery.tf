ae2c6c9d5c3fd094b8ee6cefe6c28482928ceb3a
Dave James Miller <d@djm.me>
2022-11-05 19:24:41 +0000

    Move Gallery to Seven

--- app-gallery.tf
+++ app-gallery.tf
@@ -2,5 +2,5 @@ resource "cloudflare_record" "gallery_djm_me_CNAME" {
   zone_id = cloudflare_zone.djm_me.id
   name    = "gallery"
   type    = "CNAME"
-  value   = "summer.djm.me"
+  value   = "seven.djm.me"
 }
