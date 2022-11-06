866416770e5df385f04fdf6dc55c4873ec17f364
Dave James Miller <d@djm.me>
2022-11-06 00:21:58 +0000

    Move Redmine to Seven

--- app-redmine.tf
+++ app-redmine.tf
@@ -2,5 +2,5 @@ resource "cloudflare_record" "red_djm_me_CNAME" {
   zone_id = cloudflare_zone.djm_me.id
   name    = "red"
   type    = "CNAME"
-  value   = "summer.djm.me"
+  value   = "seven.djm.me"
 }
