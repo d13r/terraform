81e22f3eb7cc66de3d2b24266345d04d1569ca85
Dave James Miller <d@djm.me>
2023-09-16 10:12:46 +0200

    Use 'private.<name>.djm.me' instead of '<name>.private.djm.me'

--- modules/rainbow_vm/main.tf
+++ modules/rainbow_vm/main.tf
@@ -16,7 +16,7 @@ resource "cloudflare_record" "public_AAAA_record" {
 
 resource "cloudflare_record" "private_A_record" {
   zone_id = var.cloudflare_zone.zone_id
-  name    = "${var.name}.private"
+  name    = "private.${var.name}"
   type    = "A"
   value   = format("192.168.150.%d", var.number)
   comment = var.managed_comment
@@ -24,7 +24,7 @@ resource "cloudflare_record" "private_A_record" {
 
 resource "cloudflare_record" "private_AAAA_record" {
   zone_id = var.cloudflare_zone.zone_id
-  name    = "${var.name}.private"
+  name    = "private.${var.name}"
   type    = "AAAA"
   value   = format("2a01:4f8:10a:2e4b:%x::", var.number)
   comment = var.managed_comment
