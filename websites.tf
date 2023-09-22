c9e5a4d436da1b22e50cd6037a602a776ffd046d
Dave James Miller <d@djm.me>
2023-09-22 08:51:22 +0200

    Remove Notes website

--- websites.tf
+++ websites.tf
@@ -7,15 +7,6 @@ resource "cloudflare_record" "c_djm_me" {
   comment = local.managed_comment
 }
 
-# Notes
-resource "cloudflare_record" "notes_djm_me" {
-  zone_id = data.cloudflare_zone.djm_me.zone_id
-  name    = "notes"
-  type    = "CNAME"
-  value   = "red.djm.me"
-  comment = local.managed_comment
-}
-
 # Photos
 resource "cloudflare_record" "photos_djm_me" {
   zone_id = data.cloudflare_zone.djm_me.zone_id
