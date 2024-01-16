7fbb318c593b6e2fb6905d28fea4ce4c26588904
Dave James Miller <d@djm.me>
2024-01-16 21:54:36 +0000

    Remove photos site

--- susan.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "susan_djm_me" {
-  zone_id = data.cloudflare_zone.djm_me.zone_id
-  name    = "susan"
-  type    = "CNAME"
-  value   = "u369343.your-storagebox.de"
-  comment = local.managed_comment
-}
--- websites.tf
+++ websites.tf
@@ -6,12 +6,3 @@ resource "cloudflare_record" "c_djm_me" {
   value   = "holly.djm.me"
   comment = local.managed_comment
 }
-
-# Photos
-resource "cloudflare_record" "photos_djm_me" {
-  zone_id = data.cloudflare_zone.djm_me.zone_id
-  name    = "photos"
-  type    = "CNAME"
-  value   = "holly.djm.me"
-  comment = local.managed_comment
-}
